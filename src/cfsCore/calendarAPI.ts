e158ae3eb1d1295c60414ec345cdac3cb888b8f7
Caffeine Power <yuto1001okuda@gmail.com>
2021-12-28 14:30:18 +0900

    [update] GAS仕様に変更

--- src/cfsCore/calendarAPI.ts
+++ src/cfsCore/calendarAPI.ts
@@ -1,7 +1,3 @@
-// Compiled using calendar-from-stream-rebuild 1.0.0 (TypeScript 4.5.4)
-var exports = exports || {};
-var module = module || { exports: exports };
-exports.insertEvent = exports.deleteEvent = exports.updateEvent = exports.getCalendarEvents = void 0;
 function getCalendarEvents(calendarId) {
     const res = Calendar.Events.list(calendarId, {
         'timeMin': new Date().toISOString(),
@@ -9,18 +5,18 @@ function getCalendarEvents(calendarId) {
         'orderBy': 'startTime'
     });
     const events = res.items;
+
     return events;
 }
-exports.getCalendarEvents = getCalendarEvents;
+
 function updateEvent(calendarId, eventId, body) {
     Calendar.Events.patch(body, calendarId, eventId);
 }
-exports.updateEvent = updateEvent;
+
 function deleteEvent(calendarId, eventId) {
     Calendar.Events.remove(calendarId, eventId);
 }
-exports.deleteEvent = deleteEvent;
+
 function insertEvent(calendarId, body) {
     Calendar.Events.insert(body, calendarId);
 }
-exports.insertEvent = insertEvent;
--- src/cfsCore/main.ts
+++ src/cfsCore/main.ts
@@ -1,17 +1,14 @@
-// Compiled using calendar-from-stream-rebuild 1.0.0 (TypeScript 4.5.4)
-var exports = exports || {};
-var module = module || { exports: exports };
-//import { getStreamsDetailFromURLs } from './youtubeAPI';
-//import { getCalendarEvents, updateEvent, deleteEvent, insertEvent } from './calendarAPI';
-//import { CALENDAR_ID, CHANNEL_LIST } from './.env';
 function main() {
-    const EinY = exports.getStreamsDetailFromURLs(exports.CHANNEL_LIST);
-    const EinC = exports.getCalendarEvents(exports.CALENDAR_ID);
-    for (let _i = 0; _i < EinY.length; _i++) {
-        const ey = EinY[_i];
+    const EinY = getStreamsDetailFromURLs(CHANNEL_LIST);
+    const EinC = getCalendarEvents(CALENDAR_ID);
+
+    for (let i = 0; i < EinY.length; i++) {
+        const ey = EinY[i];
         let isExist = false;
+
         for (let ecKey = 0; ecKey < EinC.length; ecKey++) {
             const ec = EinC[ecKey];
+
             if ('url' in ey && 'location' in ec) {
                 if (ey['url'] == ec['location']) {
                     if (ey['title'] != ec['summary'] || ey['who'] != ec['description'] || new Date(ey['startTime']).valueOf() != new Date(ec['start']['dateTime']).valueOf()) {
@@ -27,11 +24,13 @@ function main() {
                                 'dateTime': endTime.toISOString()
                             }
                         };
-                        exports.updateEvent(exports.CALENDAR_ID, ec['id'], body);
+
+                        updateEvent(CALENDAR_ID, ec['id'], body);
                         Logger.log('[update] ' + ey['title']);
-                    }else{
-                      Logger.log('[exist] ' + ey['title']);
+                    } else {
+                        Logger.log('[exist] ' + ey['title']);
                     }
+
                     EinC.splice(ecKey, 1);
                     isExist = true;
                     break;
@@ -52,13 +51,15 @@ function main() {
                     'dateTime': endTime.toISOString()
                 }
             };
-            exports.insertEvent(exports.CALENDAR_ID, body);
+
+            insertEvent(CALENDARiD, body);
             Logger.log('[insert] ' + ey['title']);
         }
     }
-    for (let _a = 0; _a < EinC.length; _a++) {
-        const ec = EinC[_a];
-        exports.deleteEvent(exports.CALENDAR_ID, ec['id']);
+    for (let i = 0; i < EinC.length; i++) {
+        const ec = EinC[i];
+
+        deleteEvent(CALENDAR_ID, ec['id']);
         Logger.log('[delete] ' + ec['summary']);
     }
 }
